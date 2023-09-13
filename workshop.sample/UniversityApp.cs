5c563fdecd5f9b719e8fbadcb1324eccf245645e
uerbzr <nigel.sibbert@gmail.com>
2023-09-13 10:11:45 +0100

    added menu/new student

--- workshop.sample/UniversityApp.cs
+++ workshop.sample/UniversityApp.cs
@@ -12,25 +12,53 @@ namespace workshop.sample
         private bool running = true;
         public UniversityApp()
         {
+
+            //instance a course - needs name + teacher
+
+
+
             string choice = string.Empty;
             while(running)
             {
+                Console.Clear();
+                MainMenu();
                 Console.WriteLine("Enter your choice...");
                 choice = Console.ReadLine();
-
                 switch (choice)
                 {
+                    case "1":
+                        Console.WriteLine($"you entered 1");
+                        FirstChoice();
+                        break;
                     case "q":
+                        Stop();
+                        break;
                     case "Q":
                         Stop();
                         break;
                     default:
+                        Console.WriteLine("you didnt enter anything??");
                         break;
                 }
             }
 
         }
 
+        private void MainMenu()
+        {
+            Console.WriteLine("Welcome to University App");
+            Console.WriteLine("1: Add student to course");
+            Console.WriteLine("q: quit");
+
+        }
+        private void FirstChoice()
+        {
+            Console.WriteLine("Enter Student Name");
+            Student student = new Student();
+            student.Name = Console.ReadLine();
+
+
+        }
         private void Stop()
         {
             running = false;
