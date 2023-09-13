f4be0ab48ddb6f1ec43ce05f12fb0c3179eb7f40
uerbzr <nigel.sibbert@gmail.com>
2023-09-13 10:00:05 +0100

    updated with app loop

--- workshop.sample/Student.cs
+++ workshop.sample/Student.cs
@@ -9,9 +9,14 @@ namespace workshop.sample
     public class Student
     {
         private string _subject = "C#";
-
+        private int _height;
         public string Name { get; set; }
         public string Subject { get { return _subject;  } set { _subject = value; } }
+
+        public int Height
+        {
+            get { return _height; }
+        }
 
         public string FavDrink()
         {
---
+++ workshop.sample/UniversityApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop.sample
+{
+    public class UniversityApp
+    {
+        private bool running = true;
+        public UniversityApp()
+        {
+            string choice = string.Empty;
+            while(running)
+            {
+                Console.WriteLine("Enter your choice...");
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "q":
+                    case "Q":
+                        Stop();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+
+        private void Stop()
+        {
+            running = false;
+        }
+    }
+}
