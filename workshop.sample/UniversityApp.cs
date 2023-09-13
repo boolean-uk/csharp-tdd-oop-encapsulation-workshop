10b78b839375f3600385887d96e231f8ddaef60d
uerbzr <nigel.sibbert@gmail.com>
2023-09-13 10:35:46 +0100

    extracted method

--- workshop.sample/UniversityApp.cs
+++ workshop.sample/UniversityApp.cs
@@ -10,11 +10,12 @@ namespace workshop.sample
     public class UniversityApp
     {
         private bool running = true;
+        private Course course;
         public UniversityApp()
         {
 
             //instance a course - needs name + teacher
-
+            course = new Course("C#", new Teacher() { FirstName = "Nigel" });
 
 
             string choice = string.Empty;
@@ -46,17 +47,39 @@ namespace workshop.sample
 
         private void MainMenu()
         {
+            Console.WriteLine("*************************");
             Console.WriteLine("Welcome to University App");
+            Console.WriteLine($"The current course is: {course.Name}");
+
+            ShowStudents();
+
             Console.WriteLine("1: Add student to course");
             Console.WriteLine("q: quit");
 
         }
+
+        private void ShowStudents()
+        {
+            if (course.Students.Any())
+            {
+                Console.WriteLine($"Students on the course:");
+                foreach (Student student in course.Students)
+                {
+                    Console.WriteLine($"--Name:{student.Name}");
+                }
+
+            }
+        }
+
         private void FirstChoice()
         {
+
             Console.WriteLine("Enter Student Name");
+            
             Student student = new Student();
             student.Name = Console.ReadLine();
 
+            course.Students.Add(student);
 
         }
         private void Stop()
