d3e8474200f4b659374f20915fd75195632ec836
uerbzr <nigel.sibbert@gmail.com>
2023-09-13 09:39:14 +0100

    updated

--- workshop.sample/Program.cs
+++ workshop.sample/Program.cs
@@ -4,38 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("********** Teacher ****************");
-
-            Teacher t1 = new Teacher();
-            t1.FirstName =  "nigel";
-            t1.Dob = new DateTime(1975, 1, 3);
-
-
-            Console.WriteLine(t1.FirstName);
-
-            Console.WriteLine(t1.Age(true));
-
-
-            Console.WriteLine("********** Student ****************");
-
-            Student s1 = new Student();
-
-            Console.WriteLine(s1.FavDrink());
-
-            Console.WriteLine(s1.Subject);
-
-
-
-            Console.WriteLine("********** Course ****************");
-
-            Course csharpCourse = new Course("C#", t1);
-            
-            csharpCourse.Students.Add(s1);
-            csharpCourse.Students.Add(new Student() { Name = "Annefleur" });
-
-            Console.WriteLine(csharpCourse.Name);
-            Console.WriteLine(csharpCourse.CourseTeacher.FirstName);
-            Console.WriteLine(csharpCourse.CourseTeacher.Age(true));
+     
         }
     }
 }
